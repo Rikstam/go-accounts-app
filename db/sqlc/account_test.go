2bba263d55d16a4c9befd65397d2675c14efd5dd
Riku Kestilä <rikukestila@gmail.com>
2022-10-14 13:54:07 +0300

    Randomise some test data in accounts test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,15 +2,16 @@ package db
 
 import (
 	"context"
+	"github.com/Rikstam/bank-app/util"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
