eb4813c94d830790c65bc834227510132341fd29
Riku Kestilä <rikukestila@gmail.com>
2022-10-21 10:03:25 +0300

    Add tests for the rest of entry queries

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -44,3 +44,41 @@ func TestGetEntry(t *testing.T) {
 	require.Equal(t, entry1.Amount, entry2.Amount)
 	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
 }
+
+func TestGetEntryWithAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+	entry2, err := testQueries.GetEntryWithAccount(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.Equal(t, account.Owner, entry2.Owner)
+	require.Equal(t, account.Balance, entry2.Balance)
+
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+	}
+}
